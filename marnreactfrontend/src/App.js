13ead1e017958aba5f5abcfa6354f354e6fd2135
unknown <ajijsarkar1@outlook.com>
2020-06-27 02:16:13 +0530

    my first commit

---
+++ marnreactfrontend/src/App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import "bootstrap/dist/css/bootstrap.min.css";
+import { BrowserRouter as Router, Route} from "react-router-dom";
+
+import Navbar from "./Components/navbar.component"
+import ExercisesList from "./Components/exercises-list.component";
+import EditExercise from "./Components/edit-exercise.component";
+import CreateExercise from "./Components/create-exercise.component";
+import CreateUser from "./Components/create-user.component";
+
+function App() {
+  return (
+    <Router>
+      <div className="container">
+      <Navbar />
+      <br/>
+      <Route path="/" exact component={ExercisesList} />
+      <Route path="/edit/:id" component={EditExercise} />
+      <Route path="/create" component={CreateExercise} />
+      <Route path="/user" component={CreateUser} />
+      </div>
+    </Router>
+  );
+}
+
+export default App;
